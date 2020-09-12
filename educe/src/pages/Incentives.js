f25c2c60bdb0c10529f4611a8f586db6ab5833b6
Oju Ajose <oajose@usc.edu>
2020-09-12 10:06:31 -0500

    Minor edits

---
+++ educe/src/pages/Incentives.js
@@ -0,0 +1,15 @@
+import React from "react"
+import 'semantic-ui-css/semantic.min.css'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Incentives from "../components/Incentives"
+
+const IncentivesPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Incentives />
+  </Layout>
+)
+
+export default IncentivesPage
